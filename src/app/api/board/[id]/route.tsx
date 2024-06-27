5c3f75bca9fc2077b78b50b798cf62fa215f14c5
cher1shRXD <taewoo080401@gmail.com>
2024-06-27 14:05:55 +0900

    fix::GET view  Api

--- src/app/api/board/[id]/route.tsx
+++ src/app/api/board/[id]/route.tsx
@@ -1,15 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
-
-export async function GET(req:NextRequest ,{ params }: { params: { id: number } }) {
-  const requestedId = params.id;
-
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const requestedId = Number(params.id);
   const prisma = new PrismaClient();
 
-  const post = await prisma.board.findUnique({
-    where: { id: requestedId },
-  });
+  try {
+    const post = await prisma.board.findUnique({
+      where: { id: requestedId },
+    });
 
-  return post;
+    if (!post) {
+      return NextResponse.json({ error: "Post not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(post);
+  } catch (error) {
+    console.error("Error fetching post:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
