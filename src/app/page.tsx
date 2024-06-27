8e8e83bef7bec48f7ea894712388435bfd45b846
cher1shRXD <taewoo080401@gmail.com>
2024-06-27 09:33:18 +0900

    feat::link to write and board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,11 @@ import Link from "next/link";
 export default function Home() {
 
   return (
-    <h1>hello world</h1>
+    <>
+      <h1>대소나무숲</h1>
+      <Link href='/write'>글 쓰기</Link>
+      <Link href='/board'>글 보기</Link>
+    </>
   );
 }
 
